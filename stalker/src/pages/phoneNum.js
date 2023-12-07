e31356bf8c2c04d7451b49fbe9f0227805c72283
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-12-07 21:56:09 +0530

    complete the structure

---
+++ stalker/src/pages/phoneNum.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PhoneNumbers = () => {
+    return (
+        <div>
+            <h1>Phone Numbers Page</h1>
+        </div>
+    );
+}
+
+export default PhoneNumbers;
