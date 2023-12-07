35c3c89a5a6970be137ea6bd62148d66cb80cbd4
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-12-07 22:08:39 +0530

    add web contents to container

--- stalker/src/App.js
+++ stalker/src/App.js
@@ -1,21 +1,20 @@
-// App.js
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
-import Home from './pages/index'; // Update the path accordingly
-import PhoneNumbers from './pages/phoneNum'; // Update the path accordingly
+import Home from './pages/index';
+import PhoneNumbers from './pages/phoneNum';
+import Container from '@mui/material/Container';
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <NavBar />
+      <NavBar />
+      <Container maxWidth="md">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/phone_numbers" element={<PhoneNumbers />} />
         </Routes>
-      </div>
+      </Container>
     </Router>
   );
 }
