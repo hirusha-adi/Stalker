8418aba19806bbcd798cc092798236c2134ac2e8
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-12-08 18:53:52 +0530

    re format everything + remove using Paper

--- stalker/src/components/phoneNum/setSettings.js
+++ stalker/src/components/phoneNum/setSettings.js
@@ -1,78 +1,105 @@
-import React, { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
-import Stack from '@mui/material/Stack';
-import SaveIcon from '@mui/icons-material/Save';
-import Collapse from '@mui/material/Collapse';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import IconButton from '@mui/material/IconButton';
-import ExpandLessIcon from '@mui/icons-material/ExpandLess';
-
-const FormItems = () => (
-    <React.Fragment>
-        <Grid container spacing={1}>
-            <Grid container item spacing={3}>
-                <Grid item>
-                    <TextField id="NUMVERIFY_API_KEY" label="NUMVERIFY_API_KEY" variant="standard" fullWidth />
-                </Grid>
-                <Grid item>
-                    <TextField id="GOOGLECSE_CX" label="GOOGLECSE_CX" variant="standard" fullWidth />
-                </Grid>
-                <Grid item>
-                    <TextField id="GOOGLE_API_KEY" label="GOOGLE_API_KEY" variant="standard" fullWidth />
-                </Grid>
-            </Grid>
-        </Grid>
-    </React.Fragment>
-);
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Stack from "@mui/material/Stack";
+import SaveIcon from "@mui/icons-material/Save";
+import Collapse from "@mui/material/Collapse";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import IconButton from "@mui/material/IconButton";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 
 const SetSettings = () => {
-    const [collapsed, setCollapsed] = useState(true);
+  const [collapsed, setCollapsed] = useState(true);
 
-    const handleToggleCollapse = () => {
-        setCollapsed(!collapsed);
-    };
+  const handleToggleCollapse = () => {
+    setCollapsed(!collapsed);
+  };
 
-    const FormTitle = () => (
-        <h2 onClick={handleToggleCollapse} style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
-            Update Settings
-            <IconButton size="large" onClick={handleToggleCollapse}>
-                {collapsed ? (
-                    <ExpandMoreIcon fontSize="inherit" />
-                ) : (
-                    <ExpandLessIcon fontSize="inherit" />
-                )}
-            </IconButton>
-        </h2>
-    );
+  const FormTitle = () => (
+    <h2
+      onClick={handleToggleCollapse}
+      style={{ cursor: "pointer", display: "flex", alignItems: "center" }}
+    >
+      Update Settings
+      <IconButton size="large" onClick={handleToggleCollapse}>
+        {collapsed ? (
+          <ExpandMoreIcon fontSize="inherit" />
+        ) : (
+          <ExpandLessIcon fontSize="inherit" />
+        )}
+      </IconButton>
+    </h2>
+  );
 
-    return (
-        <div>
-            <Paper elevation={3} sx={{ p: 2, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <div>
-                    <FormTitle />
-                    <Collapse in={!collapsed}>
-                        <div>
-                            <FormItems />
-                            <br />
-                            <Stack direction="row" spacing={2} sx={{ p: 2, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                                <Button variant="outlined" color="error" startIcon={<DeleteIcon />}>
-                                    Clear
-                                </Button>
-                                <Button variant="contained" endIcon={<SaveIcon />}>
-                                    Update
-                                </Button>
-                            </Stack>
-                        </div>
-                    </Collapse>
-                </div>
-            </Paper>
-        </div>
+  const FormItems = () => (
+    <React.Fragment>
+      <Grid container spacing={1} justifyContent="flex-end">
+        <Grid container item spacing={4}>
+          <Grid item xs={12} md={4}>
+            <TextField
+              id="NUMVERIFY_API_KEY"
+              label="NUMVERIFY_API_KEY"
+              variant="standard"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <TextField
+              id="GOOGLECSE_CX"
+              label="GOOGLECSE_CX"
+              variant="standard"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <TextField
+              id="GOOGLE_API_KEY"
+              label="GOOGLE_API_KEY"
+              variant="standard"
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
 
-    );
-}
+  return (
+    <div>
+      <div>
+        <FormTitle />
+        <Collapse in={!collapsed}>
+          <div>
+            <FormItems />
+            <br />
+            <Stack
+              direction="row"
+              spacing={2}
+              sx={{
+                p: 2,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Button
+                variant="outlined"
+                color="error"
+                startIcon={<DeleteIcon />}
+              >
+                Clear
+              </Button>
+              <Button variant="contained" endIcon={<SaveIcon />}>
+                Update
+              </Button>
+            </Stack>
+          </div>
+        </Collapse>
+      </div>
+    </div>
+  );
+};
 
 export default SetSettings;
