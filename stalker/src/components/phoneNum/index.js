90b81d15784addaf2ca7dbde76acfbb724670026
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-12-08 20:06:22 +0530

    show table

--- stalker/src/components/phoneNum/index.js
+++ stalker/src/components/phoneNum/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import SetSettings from "./setSettings";
 import IntroInformation from "./introInfomation";
+import ShowGoogleDorks from "./showGoogleDorks";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import SearchIcon from "@mui/icons-material/Search";
 import IconButton from "@mui/material/IconButton";
 
 const phoneNum = () => {
+  const googleDorksList = [
+    { id: 1, lastName: "Hirusha", firstName: "Jon", age: 35 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  ];
+
   const SearchNumber = () => (
     <div style={{ textAlign: "center" }}>
       <h2>Search Phone Number</h2>
@@ -47,6 +60,9 @@ const phoneNum = () => {
       <br></br>
       <SearchNumber />
       <SetSettings />
+      <hr></hr>
+      <br></br>
+      <ShowGoogleDorks rows={googleDorksList} />
       <br></br>
       <br></br>
       <hr></hr>
