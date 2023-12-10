4296aba9f904b5d7aad19e4d9104d1cf4443693c
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-12-10 12:53:00 +0530

    move title to index

--- stalker/src/components/phoneNum/showInformation.js
+++ stalker/src/components/phoneNum/showInformation.js
@@ -7,35 +7,32 @@ import TableRow from "@mui/material/TableRow";
 
 const ShowInformation = ({ data }) => {
   return (
-    <>
-      <h2>Number Information</h2>
-      <TableContainer sx={{ marginTop: 2 }}>
-        <Table>
-          <TableBody>
-            <TableRow>
-              <TableCell>Raw Local</TableCell>
-              <TableCell>{data.rawLocal}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Local</TableCell>
-              <TableCell>{data.local}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>E164</TableCell>
-              <TableCell>{data.e164}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>International</TableCell>
-              <TableCell>{data.international}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Country</TableCell>
-              <TableCell>{data.country}</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
+    <TableContainer sx={{ marginTop: 2 }}>
+      <Table>
+        <TableBody>
+          <TableRow>
+            <TableCell>Raw Local</TableCell>
+            <TableCell>{data.rawLocal}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Local</TableCell>
+            <TableCell>{data.local}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>E164</TableCell>
+            <TableCell>{data.e164}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>International</TableCell>
+            <TableCell>{data.international}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Country</TableCell>
+            <TableCell>{data.country}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
