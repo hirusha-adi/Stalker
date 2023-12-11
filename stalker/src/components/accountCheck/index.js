a98b4c59b0217cf6cc96b041d39c2f64a7c178e4
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-12-11 10:35:34 -0800

    use the new modular ShowSectionHeader

--- stalker/src/components/accountCheck/index.js
+++ stalker/src/components/accountCheck/index.js
@@ -12,121 +12,95 @@ import SearchIcon from "@mui/icons-material/Search";
 // my components
 import IntroInformation from "./introInfomation";
 import SetSettings from "./setSettings";
-
+import ShowSecitonHeader from "../_shared/sectionHeader";
 
 import Button from "@mui/material/Button";
 import DeleteIcon from "@mui/icons-material/Delete";
 import Stack from "@mui/material/Stack";
 
-const ShowSecitonHeader = () => (
-  <>
-    <br></br>
-    {/* Phoneinfoga logo */}
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <img
-        src="https://user-images.githubusercontent.com/27065646/53551960-ae4dff80-3b3a-11e9-9075-cef786c69364.png"
-        alt="banner"
-        style={{ alignSelf: "center", width: "20%" }}
-      />
-      <br></br>
-    </div>
-    <br></br>
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <h3>
-        Hunt down social media accounts by username across social networks
-      </h3>
-    </div>
-    <br></br>
-  </>
-);
-
 const AccountCheck = () => {
-  // const phoneinfogaData = {
-  //   status: {
-  //     error: false,
-  //     error_desc: "",
-  //     show_information: true,
-  //   },
-  //   accountsList: {},
-  // };
+	// const phoneinfogaData = {
+	//   status: {
+	//     error: false,
+	//     error_desc: "",
+	//     show_information: true,
+	//   },
+	//   accountsList: {},
+	// };
 
-  const SearchUsername = () => (
-    <div style={{ textAlign: "center" }}>
-      <h2>Search Username</h2>
-      <Grid container spacing={1} alignItems="center" justifyContent="center">
-        <Grid item xs={11}>
-          <TextField
-            id="USERNAME"
-            label="Input Username"
-            variant="standard"
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={1}>
-          <IconButton aria-label="search" size="large">
-            <SearchIcon fontSize="inherit" />
-          </IconButton>
-        </Grid>
-      </Grid>
-    </div>
-  );
+	const SearchUsername = () => (
+		<div style={{ textAlign: "center" }}>
+			<h2>Search Username</h2>
+			<Grid
+				container
+				spacing={1}
+				alignItems="center"
+				justifyContent="center"
+			>
+				<Grid item xs={11}>
+					<TextField
+						id="USERNAME"
+						label="Input Username"
+						variant="standard"
+						fullWidth
+					/>
+				</Grid>
+				<Grid item xs={1}>
+					<IconButton aria-label="search" size="large">
+						<SearchIcon fontSize="inherit" />
+					</IconButton>
+				</Grid>
+			</Grid>
+		</div>
+	);
 
-  const ButtonsSubmitClear = () => (
-    <Stack
-      direction="row"
-      spacing={2}
-      sx={{
-        p: 2,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Button
-        variant="outlined"
-        color="error"
-        startIcon={<DeleteIcon />}
-      >
-        Clear
-      </Button>
-      <Button variant="contained" endIcon={<SearchIcon />}>
-        Search
-      </Button>
-    </Stack>
-  );
+	const ButtonsSubmitClear = () => (
+		<Stack
+			direction="row"
+			spacing={2}
+			sx={{
+				p: 2,
+				display: "flex",
+				alignItems: "center",
+				justifyContent: "center",
+			}}
+		>
+			<Button variant="outlined" color="error" startIcon={<DeleteIcon />}>
+				Clear
+			</Button>
+			<Button variant="contained" endIcon={<SearchIcon />}>
+				Search
+			</Button>
+		</Stack>
+	);
 
-  return (
-    <div>
-      {/* Section Header */}
-      <ShowSecitonHeader />
+	return (
+		<div>
+			{/* Section Header */}
+			<ShowSecitonHeader
+				subtitle="Hunt down social media accounts by username across social networks"
+				width="20%"
+				imgURL="https://user-images.githubusercontent.com/27065646/53551960-ae4dff80-3b3a-11e9-9075-cef786c69364.png"
+			/>
 
-      {/* Search Bar (also includes the search button) */}
-      <SearchUsername />
+			{/* Search Bar (also includes the search button) */}
+			<SearchUsername />
 
-      {/* Advanced Settings */}
-      <SetSettings />
+			{/* Advanced Settings */}
+			<SetSettings />
 
-      {/* Clear + Update Buttons */}
-      <ButtonsSubmitClear />
+			{/* Clear + Update Buttons */}
+			<ButtonsSubmitClear />
 
-      {/* About sherlock */}
-      <br /><br /><hr />
-      <IntroInformation />
-      <br /><br />
-    </div>
-  );
+			{/* About sherlock */}
+			<br />
+			<br />
+			<hr />
+			<IntroInformation />
+			<br />
+			<br />
+		</div>
+	);
 };
 
 export default AccountCheck;
