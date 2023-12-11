6acffff5ae9f8ec2be35b5f7e0d0cdfe175b5b08
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-12-11 10:42:28 -0800

    fix formatting

--- stalker/src/components/_shared/sectionHeader.js
+++ stalker/src/components/_shared/sectionHeader.js
@@ -1,30 +1,26 @@
 import React from "react";
 
 const ShowSectionHeader = ({ width, imgURL, subtitle }) => {
-	const centeredStyle = {
-		display: "flex",
-		justifyContent: "center",
-		alignItems: "center",
-	};
+  const centeredStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
 
-	return (
-		<>
-			<br />
-			{/* Center the image */}
-			<div style={centeredStyle}>
-				{/* Image (logo) */}
-				<img
-					src={imgURL}
-					alt="banner"
-					style={{ alignSelf: "center", width }}
-				/>
-				<br />
-			</div>
-			<br />
-			<div style={centeredStyle}>{subtitle && <h3>{subtitle}</h3>}</div>
-			<br />
-		</>
-	);
+  return (
+    <>
+      <br />
+      {/* Center the image */}
+      <div style={centeredStyle}>
+        {/* Image (logo) */}
+        <img src={imgURL} alt="banner" style={{ alignSelf: "center", width }} />
+        <br />
+      </div>
+      <br />
+      <div style={centeredStyle}>{subtitle && <h3>{subtitle}</h3>}</div>
+      <br />
+    </>
+  );
 };
 
 export default ShowSectionHeader;
