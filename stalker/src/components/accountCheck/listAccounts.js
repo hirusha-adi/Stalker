2cf978a78e298a67feacc3d7f23c72b63d808f74
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-12-11 11:49:08 -0800

    improve

--- stalker/src/components/accountCheck/listAccounts.js
+++ stalker/src/components/accountCheck/listAccounts.js
@@ -2,27 +2,24 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 import { DataGrid } from "@mui/x-data-grid";
 
-const AvailableAccountsTable = ({ title, rows }) => {
-  const columns = [
-    { field: "username", headerName: "Username", flex: 1 },
-    { field: "name", headerName: "Name", flex: 1 },
-    { field: "url_main", headerName: "Main URL", flex: 1 },
-    { field: "url_user", headerName: "User URL", flex: 1 },
-    { field: "exists", headerName: "Exists", flex: 1 },
-    { field: "http_status", headerName: "HTTP Status", flex: 1 },
-    { field: "response_time_s", headerName: "Response Time (s)", flex: 1 },
-  ];
+const columns = [
+  { field: "username", headerName: "Username", flex: 1 },
+  { field: "name", headerName: "Name", flex: 1 },
+  { field: "url_main", headerName: "Main URL", flex: 1 },
+  { field: "url_user", headerName: "User URL", flex: 1 },
+  { field: "exists", headerName: "Exists", flex: 1 },
+  { field: "http_status", headerName: "HTTP Status", flex: 1 },
+  { field: "response_time_s", headerName: "Response Time (s)", flex: 1 },
+];
 
-  return (
+// const AvailableAccountsTable = ({ title, rows }) =>
+const AvailableAccountsTable = ({ rows }) =>
+  rows.length === 0 ? null : (
     <>
-      <Typography variant="h5" sx={{ marginTop: 3, marginBottom: 2 }}>
-        {title}
-      </Typography>
       <div style={{ height: 400, width: "100%" }}>
         <DataGrid
           rows={rows}
           columns={columns}
-          pageSize={5}
           initialState={{
             pagination: {
               paginationModel: { page: 0, pageSize: 5 },
@@ -34,6 +31,5 @@ const AvailableAccountsTable = ({ title, rows }) => {
       </div>
     </>
   );
-};
 
 export default AvailableAccountsTable;
