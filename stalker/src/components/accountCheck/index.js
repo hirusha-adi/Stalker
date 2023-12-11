67e50dc19a53d2eb60b0821d90a76a0adaf87261
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-12-11 10:43:50 -0800

    fix formatting

--- stalker/src/components/accountCheck/index.js
+++ stalker/src/components/accountCheck/index.js
@@ -19,88 +19,83 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import Stack from "@mui/material/Stack";
 
 const AccountCheck = () => {
-	// const phoneinfogaData = {
-	//   status: {
-	//     error: false,
-	//     error_desc: "",
-	//     show_information: true,
-	//   },
-	//   accountsList: {},
-	// };
+  // const phoneinfogaData = {
+  //   status: {
+  //     error: false,
+  //     error_desc: "",
+  //     show_information: true,
+  //   },
+  //   accountsList: {},
+  // };
 
-	const SearchUsername = () => (
-		<div style={{ textAlign: "center" }}>
-			<h2>Search Username</h2>
-			<Grid
-				container
-				spacing={1}
-				alignItems="center"
-				justifyContent="center"
-			>
-				<Grid item xs={11}>
-					<TextField
-						id="USERNAME"
-						label="Input Username"
-						variant="standard"
-						fullWidth
-					/>
-				</Grid>
-				<Grid item xs={1}>
-					<IconButton aria-label="search" size="large">
-						<SearchIcon fontSize="inherit" />
-					</IconButton>
-				</Grid>
-			</Grid>
-		</div>
-	);
+  const SearchUsername = () => (
+    <div style={{ textAlign: "center" }}>
+      <h2>Search Username</h2>
+      <Grid container spacing={1} alignItems="center" justifyContent="center">
+        <Grid item xs={11}>
+          <TextField
+            id="USERNAME"
+            label="Input Username"
+            variant="standard"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={1}>
+          <IconButton aria-label="search" size="large">
+            <SearchIcon fontSize="inherit" />
+          </IconButton>
+        </Grid>
+      </Grid>
+    </div>
+  );
 
-	const ButtonsSubmitClear = () => (
-		<Stack
-			direction="row"
-			spacing={2}
-			sx={{
-				p: 2,
-				display: "flex",
-				alignItems: "center",
-				justifyContent: "center",
-			}}
-		>
-			<Button variant="outlined" color="error" startIcon={<DeleteIcon />}>
-				Clear
-			</Button>
-			<Button variant="contained" endIcon={<SearchIcon />}>
-				Search
-			</Button>
-		</Stack>
-	);
+  const ButtonsSubmitClear = () => (
+    <Stack
+      direction="row"
+      spacing={2}
+      sx={{
+        p: 2,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Button variant="outlined" color="error" startIcon={<DeleteIcon />}>
+        Clear
+      </Button>
+      <Button variant="contained" endIcon={<SearchIcon />}>
+        Search
+      </Button>
+    </Stack>
+  );
 
-	return (
-		<div>
-			{/* Section Header */}
-			<ShowSecitonHeader
-				subtitle="Hunt down social media accounts by username across social networks"
-				width="20%"
-				imgURL="https://user-images.githubusercontent.com/27065646/53551960-ae4dff80-3b3a-11e9-9075-cef786c69364.png"
-			/>
+  return (
+    <div>
+      {/* Section Header */}
+      <ShowSecitonHeader
+        subtitle="Hunt down social media accounts by username across social networks"
+        width="20%"
+        imgURL="https://user-images.githubusercontent.com/27065646/53551960-ae4dff80-3b3a-11e9-9075-cef786c69364.png"
+      />
 
-			{/* Search Bar (also includes the search button) */}
-			<SearchUsername />
+      {/* Search Bar (also includes the search button) */}
+      <SearchUsername />
 
-			{/* Advanced Settings */}
-			<SetSettings />
+      {/* Advanced Settings */}
+      <SetSettings />
 
-			{/* Clear + Update Buttons */}
-			<ButtonsSubmitClear />
+      {/* Clear + Update Buttons */}
+      <ButtonsSubmitClear />
 
-			{/* About sherlock */}
-			<br />
-			<br />
-			<hr />
-			<IntroInformation />
-			<br />
-			<br />
-		</div>
-	);
+      {/* About sherlock */}
+      <br />
+      <br />
+      <hr />
+      <IntroInformation />
+      <br />
+      <br />
+    </div>
+  );
 };
 
 export default AccountCheck;
