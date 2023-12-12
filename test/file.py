6dc1f27a48c904d79b5842a21945b8890d901daa
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-12-12 10:42:44 +0530

    add to dictionary

--- test/file.py
+++ test/file.py
@@ -2,6 +2,8 @@ import requests
 import json
 from concurrent.futures import ThreadPoolExecutor
 
+found_accounts = []
+
 def check_username_on_site(site, username):
     uri = site.get("uri_check")
     method = site.get("method", "GET")
@@ -14,6 +16,17 @@ def check_username_on_site(site, username):
         response = requests.post(uri, data=payload, headers=headers)
 
     if response.status_code == site["e_code"] and site["e_string"] in response.text:
+        account_info = {
+            "id": len(found_accounts) + 1,
+            "username": username,
+            "name": site.get("name"),
+            "url_main": site.get("url_main", ""),
+            "url_user": site.get("url_user", ""),
+            "exists": "Claimed",
+            "http_status": response.status_code,
+            "response_time_s": f"{response.elapsed.total_seconds():.3f}",
+        }
+        found_accounts.append(account_info)
         print(f"Username {username} found on {site['name']} ({site['cat']} category)")
         return True
     elif response.status_code == site["m_code"] and site["m_string"] in response.text:
@@ -30,6 +43,10 @@ def check_username(username):
 
     if not any(results):
         print(f"Username {username} not found on any site.")
+    else:
+        print("\nFound Accounts:")
+        for account_info in found_accounts:
+            print(account_info)
 
 if __name__ == "__main__":
     username_to_check = input("Enter the username to check: ")
