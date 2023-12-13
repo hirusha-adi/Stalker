8024862987809dde739fa354daa0f9714c5eed8b
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-12-13 03:20:46 -0800

    add `phone_num` to swagger

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flasgger import Swagger
+from flasgger import swag_from
 
 from modules import PhoneNumberLookup
 
@@ -22,11 +23,85 @@ swagger_config = {
     "specs_route": "/"
 }
 
+api.config['SWAGGER'] = {
+    'title': 'Phone Number Information API',
+    'uiversion': 3,
+}
+
 swagger = Swagger(api, config=swagger_config)
 
 @api.route('/phone_num', methods=['POST'])
-def phone_number_information():
 
+def phone_number_information():
+    """
+    Endpoint to get information about a phone number.
+
+    ---
+    parameters:
+      - name: PHONE_NUMBER
+        in: formData
+        type: string
+        required: true
+        description: The phone number to look up.
+
+    responses:
+      200:
+        description: Successful response
+        schema:
+          type: object
+          properties:
+            status:
+              type: object
+              properties:
+                error:
+                  type: boolean
+                error_desc:
+                  type: array
+                  items:
+                    type: string
+                show_information:
+                  type: boolean
+                show_scanner_googlesearch:
+                  type: boolean
+            information:
+              type: object
+              properties:
+                raw_local:
+                  type: string
+                local:
+                  type: string
+                e164:
+                  type: string
+                international:
+                  type: string
+                country:
+                  type: string
+            scanner_googlesearch:
+              type: object
+              properties:
+                social_media:
+                  type: array
+                  items:
+                    type: string
+                disposable_providers:
+                  type: array
+                  items:
+                    type: string
+                reputation:
+                  type: array
+                  items:
+                    type: string
+                individuals:
+                  type: array
+                  items:
+                    type: string
+                general:
+                  type: array
+                  items:
+                    type: string
+      400:
+        description: Phone number not provided in the request.
+    """
     phone_number = request.form.get('PHONE_NUMBER')  
 
     if phone_number:
