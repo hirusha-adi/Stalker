cd15308062563b4cdd900230414ec3e7551338f6
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-12-13 11:34:54 +0530

    swagger ui is now at + others are very incomplete

--- backend/app.py
+++ backend/app.py
@@ -3,14 +3,94 @@ from flasgger import Swagger
 
 
 api = Flask(__name__)
-swagger = Swagger(api, template_file='swagger_template.yml', url='/')
+swagger_config = {
+    "headers": [
+    ],
+    "specs": [
+        {
+            "endpoint": 'apispec_1',
+            "route": '/apispec_1.json',
+            "rule_filter": lambda rule: True,  # all in
+            "model_filter": lambda tag: True,  # all in
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    # "static_folder": "static",  # must be set by user
+    "swagger_ui": True,
+    "specs_route": "/"
+}
+
+swagger = Swagger(api, config=swagger_config)
 
 
-@api.route('/profile')
-def my_profile():
-    response_body = {
-        "name": "Nagato",
-        "about" :"Hello! I'm a full stack developer that loves python and javascript"
+
+@api.route('/phone_num')
+def phone_number_information():
+
+    """
+    phoneinfogaData = {
+    status: {
+      error: false,
+      error_desc: "",
+      show_information: true,
+      show_GoogleDorks: true,
+    },
+    information: {
+      rawLocal: "0713395547",
+      local: "071 339 5547",
+      e164: "+94713395547",
+      international: "94713395547",
+      country: "LK",
+    },
+    googleDorks: {
+      socialMedia: [
+        { id: 1, lastName: "Hirusha", firstName: "Jon", age: 35 },
+        { id: 2, lastName: "Hirusha", firstName: "Jon", age: 35 },
+      ],
+      disposableProviders: [
+        {
+          id: 5,
+          lastName: "Targaryen",
+          firstName: "Daenerys",
+          age: null,
+        },
+        {
+          id: 6,
+          lastName: "Targaryen",
+          firstName: "Daenerys",
+          age: null,
+        },
+      ],
+      reputation: [
+        { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+        { id: 3, lastName: "Lannister", firstName: "Cersei", age: 42 },
+      ],
+      individuals: [
+        { id: 4, lastName: "Hirusha", firstName: "Jon", age: 35 },
+        { id: 5, lastName: "Hirusha", firstName: "Jon", age: 35 },
+      ],
+      general: [
+        { id: 8, lastName: "Stark", firstName: "Arya", age: 16 },
+        { id: 9, lastName: "Stark", firstName: "Arya", age: 16 },
+      ],
+    },
+  };
+
+    """
+    final_data = {
+        'status': {
+        'error': False,
+        'error_desc': "",
+        'show_information': True,
+        'show_GoogleDorks': True,
+      },
+      'information': {
+        'rawLocal': "0713395547",
+        'local': "071 339 5547",
+        'e164': "+94713395547",
+        'international': "94713395547",
+        'country': "LK",
+      },
     }
 
-    return response_body
+    return final_data
