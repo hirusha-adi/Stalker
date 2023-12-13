e1d4d282f0184157179537fe01002815dd889a92
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-12-13 01:59:31 -0800

    remove unwanted code

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask import jsonify
 from flasgger import Swagger
 
+from modules import PhoneNumberLookup
 
 api = Flask(__name__)
 swagger_config = {
@@ -22,69 +24,10 @@ swagger_config = {
 
 swagger = Swagger(api, config=swagger_config)
 
-
-
 @api.route('/phone_num')
 def phone_number_information():
 
-    """
-    phoneinfogaData = {
-    status: {
-      error: false,
-      error_desc: "",
-      show_information: true,
-      show_GoogleDorks: true,
-    },
-    information: {
-      rawLocal: "0713395547",
-      local: "071 339 5547",
-      e164: "+94713395547",
-      international: "94713395547",
-      country: "LK",
-    },
-    googleDorks: {
-      socialMedia: [
-        { id: 1, url: "" },
-        { id: 2, url: "" },
-      ],
-      disposableProviders: [
-        { id: 1, url: "" },
-        { id: 2, url: "" },
-      ],
-      reputation: [
-        { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-        { id: 3, lastName: "Lannister", firstName: "Cersei", age: 42 },
-      ],
-      individuals: [
-        { id: 4, lastName: "Hirusha", firstName: "Jon", age: 35 },
-        { id: 5, lastName: "Hirusha", firstName: "Jon", age: 35 },
-      ],
-      general: [
-        { id: 8, lastName: "Stark", firstName: "Arya", age: 16 },
-        { id: 9, lastName: "Stark", firstName: "Arya", age: 16 },
-      ],
-    },
-  };
-
-    """
-    final_data = {
-        'status': {
-        'error': False,
-        'error_desc': "",
-        'show_information': True,
-        'show_GoogleDorks': True,
-      },
-      'information': {
-        'rawLocal': "0713395547",
-        'local': "071 339 5547",
-        'e164': "+94713395547",
-        'international': "94713395547",
-        'country': "LK",
-      },
-    }
-
-    return final_data
-
-if __name__ == "__main__":
-  from modules import PhoneNumberLookup
-  obj = PhoneNumberLookup.test("+94713395547")
+    obj = PhoneNumberLookup("+94713395547")
+    result = obj.run()
+    # print(result)
+    return result
