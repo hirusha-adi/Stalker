efeaaaa354c94a3fd2618cf418a122eb2d2c2d10
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-12-20 07:57:00 -0800

    make it even faster using session

--- test/file.py
+++ test/file.py
@@ -1,10 +1,9 @@
 import requests
 import json
 from concurrent.futures import ThreadPoolExecutor
+from urllib.parse import urlparse
 
 found_accounts = []
-
-from urllib.parse import urlparse
 
 def extract_main_url(input_url):
     try:
@@ -14,7 +13,7 @@ def extract_main_url(input_url):
     except:
         return input_url 
 
-def check_username_on_site(site, username):
+def check_username_on_site(site, username, session):
     uri = site.get("uri_check")
     method = site.get("method", "GET")
     payload = site.get("post_body", {})
@@ -23,12 +22,12 @@ def check_username_on_site(site, username):
     try:
         if method == "GET":
             final_url = uri.format(account=username)
-            response = requests.get(final_url, headers=headers, timeout=10)
+            response = session.get(final_url, headers=headers, timeout=10)
         elif method == "POST":
             final_url = uri
-            response = requests.post(final_url, data=payload, headers=headers, timeout=10)
+            response = session.post(final_url, data=payload, headers=headers, timeout=10)
 
-        response.raise_for_status()  # Raise HTTPError for bad responses
+        response.raise_for_status()
 
         if response.status_code == site["e_code"] and site["e_string"] in response.text:
             account_info = {
@@ -42,19 +41,12 @@ def check_username_on_site(site, username):
                 "response_time_s": f"{response.elapsed.total_seconds():.3f}",
             }
             found_accounts.append(account_info)
-            print(f"Username {username} found on {site['name']} ({site['cat']} category)")
             return True
         elif response.status_code == site["m_code"] and site["m_string"] in response.text:
-            print(f"Username {username} not found on {site['name']} ({site['cat']} category)")
+            return False
 
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
-    except requests.exceptions.ConnectionError as conn_err:
-        print(f"Connection error occurred: {conn_err}")
-    except requests.exceptions.Timeout as timeout_err:
-        print(f"Request timed out: {timeout_err}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+    except requests.exceptions.RequestException as req_err:
+        print(f"Error occurred for {site['name']} - {req_err}")
 
     return False
 
@@ -62,8 +54,8 @@ def check_username(username):
     with open('wmn-data.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
 
-    with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(check_username_on_site, site, username) for site in data["sites"]]
+    with ThreadPoolExecutor() as executor, requests.Session() as session:
+        futures = [executor.submit(check_username_on_site, site, username, session) for site in data["sites"]]
         results = [future.result() for future in futures]
 
     if not any(results):
