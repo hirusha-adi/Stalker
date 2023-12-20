98343b57605f55296b7030d4ce9cdbc095d1119a
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-12-20 09:25:33 -0800

    describe functions

--- backend/utils/errors.py
+++ backend/utils/errors.py
@@ -1,4 +1,10 @@
 
 class PhoneinfogaNotFoundError(Exception):
+    """
+    Exception raised when the Phoneinfoga executable is not found.
+
+    This exception is raised when attempting to execute Phoneinfoga, but the required executable
+    (phoneinfoga.exe) cannot be found in the specified location.
+    """
     pass
 
