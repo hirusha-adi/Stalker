750ca03f36e309f35818a07d71692e8ba969bae7
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2024-04-18 10:46:04 +0530

    name directory

--- modules/username/_handle.py
+++ modules/username/_handle.py
@@ -1,4 +1,5 @@
 from . import lookup
+from . import directory
 
 def handler(sub_modules, args, is_interactive=True):
     
@@ -11,4 +12,16 @@ def handler(sub_modules, args, is_interactive=True):
             username = args[0]
         print(f"[*] Using username: {username}")
         lookup.start(username=username)
+    
+    elif sub_modules_str == "directory":
+        if is_interactive:
+            name = input("[?] Username: ")
+            first_or_last = input("[?] First or Last name? [^first/last]: ")
+        else:
+            name = args[0]
+            first_or_last = args[1]
+        print(f"[*] Using name: {name}")
+        print(f"[*] Using name as {first_or_last} name")
+        directory.start(name=name, first_or_last=first_or_last)
+        
         
