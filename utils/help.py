3f88102b635d4c06d52d30c2be67e268f02160c7
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2024-04-18 11:53:45 +0530

    remove ugly help

--- utils/help.py
+++ utils/help.py
@@ -3,11 +3,5 @@ def main_help():
     print("""
 Program usage: 
     python3 stalker.py <module> [arguments]
-
-<module>
-    Always required.
-    Available modules:
-        help
-        username
-        
           """)
+    
