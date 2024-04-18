a83bcba42c2feb19614a075d1e0e6d84828eba7d
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2024-04-18 10:08:52 +0530

    pass in to the username module

--- start.py
+++ start.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+from modules import username
+
 args =  sys.argv[:]
 
 try:
@@ -10,10 +12,18 @@ except IndexError:
     # display_help()
     sys.exit()
 
+is_interactive = False
 try:
     args_to_pass = args[2:]
     if len(args_to_pass) == 0:
         raise IndexError
 except IndexError:
+    is_interactive = True
     print("[+] No arguments passed, starting interactive mode")
 
+modules = module.split("/")
+main_module = modules[0]
+sub_modules = modules[1:]
+
+if main_module == "username":
+    username.handler(sub_modules=sub_modules, args=args_to_pass, is_interactive=is_interactive)
