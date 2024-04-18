50cdbb105b88fa277934758ea9d649c6bb7410a1
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2024-04-18 11:24:39 +0530

    not implemented yet

--- modules/username/__init__.py
+++ modules/username/__init__.py
@@ -7,6 +7,11 @@ Sub-modules ->
     username/search/directory - Names directory
     username/specific/amazon - Amazon username search
     username/specific/github - Github username search
+
+Not implemented yet (To-DO) ->
+    username/specific/tinder - Tinder username search
+    username/specific/pgp - MIT PGP Key Server
+
 """
 
 from ._handle import handler
