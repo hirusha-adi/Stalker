c1deeaaf3d1f7e92ed7b05e104258bbe600469f4
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2024-04-19 16:50:52 +0530

    add decriptions

--- modules/email/__init__.py
+++ modules/email/__init__.py
@@ -1,11 +1,25 @@
 """
-Module Name: `username`
+Module Name: `email`
 
 Modules List
 ------------
 
 Sub-modules ->
-    username/specific/github - Users/Organizations associated with emails
+    email/specific/github - Users/Organizations associated with emails
+
+
+Modules Help
+------------
+
+email/specific/github
+    - Description
+        Use GitHub's official API to get accounts and organizations
+        associated with the given email address.
+    - Usage
+        python stalker.py "email/specific/github" <email>
+    - Example
+        python stalker.py "email/specific/github" "johndoe@gmail.com"
+        python stalker.py "email/specific/github" "janedoe@protonmail.com"
 
 """
 
