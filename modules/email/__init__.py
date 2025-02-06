d741c690fc62de0454115c4581617b369879887c
hirusha-adi <hirushaadi@gmail.com>
2025-02-06 18:32:50 +0530

    imports

--- modules/email/__init__.py
+++ modules/email/__init__.py
@@ -1,46 +1,13 @@
-"""
-Module Name: `email`
-
-Modules List
-------------
-
-Sub-modules ->
-    email/specific/github - Users/Organizations associated with the email
-    email/specific/chess_com - Check if account exists under this email
-    email/specific/deezer - Check if account exists under this email
-
-
-Modules Help
-------------
-
-email/specific/github
-    - Description
-        Use GitHub's official API to get accounts and organizations
-        associated with the given email address.
-    - Usage
-        python stalker.py "email/specific/github" <email>
-    - Example
-        python stalker.py "email/specific/github" "johndoe@gmail.com"
-        python stalker.py "email/specific/github" "janedoe@protonmail.com"
-
-email/specific/chess_com
-    - Description
-        CHeck if an account exists for the given email address
-    - Usage
-        python stalker.py "email/specific/chess_com" <email>
-    - Example
-        python stalker.py "email/specific/chess_com" "johndoe@gmail.com"
-        python stalker.py "email/specific/chess_com" "janedoe@protonmail.com"
-
-email/specific/deezer
-    - Description
-        CHeck if an account exists for the given email address
-    - Usage
-        python stalker.py "email/specific/deezer" <email>
-    - Example
-        python stalker.py "email/specific/deezer" "johndoe@gmail.com"
-        python stalker.py "email/specific/deezer" "janedoe@protonmail.com"
-
-"""
-
-from ._handle import handler
+from .specific import chess_com
+from .specific import deezer
+from .specific import duolingo
+from .specific import github
+from .specific import gravatar
+from .specific import imgur
+from .specific import instagram
+from .specific import pinterest
+from .specific import pornhub
+from .specific import spotify
+from .specific import strava
+from .specific import twitter
+from . import holehe_
