c983be58aa7995678ac28e5f67487895b05188f0
hirusha-adi <hirushaadi@gmail.com>
2025-02-06 18:14:20 +0530

    improve error handling

--- utils/errors.py
+++ utils/errors.py
@@ -1,29 +1,14 @@
+import traceback
+from functools import wraps
+
 def handle_errors(func):
-    """
-    A decorator to handle errors gracefully in functions.
-
-    Args:
-        func (function): The function to be decorated.
-
-    Returns:
-        function: The decorated function.
-
-    Raises:
-        None
-
-    Example:
-        @handle_errors
-        def my_function():
-            try:
-                # code that might raise an exception
-            except Exception as e:
-                # handle the exception gracefully
-    """
-
+    @wraps(func)
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         except Exception as e:
-            print(f"An error occurred: {e}")
+            print(f"An error occurred in {func.__name__}: {e}")
+            traceback.print_exc() 
+            return None 
 
     return wrapper
